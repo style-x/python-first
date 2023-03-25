464b2d5583cbd8d73f679861a188c49e64eb2a6f
xan <47989552+style-x@users.noreply.github.com>
2023-03-25 13:02:23 +0100

    Mean-Variance-Standard Deviation Calculator

---
+++ main.py
@@ -0,0 +1,4 @@
+import NumPy 
+
+def calculate()
+
---
+++ mean_var_std.py
@@ -0,0 +1,9 @@
+
+{
+  'mean': [axis1, axis2, flattened],
+  'variance': [axis1, axis2, flattened],
+  'standard deviation': [axis1, axis2, flattened],
+  'max': [axis1, axis2, flattened],
+  'min': [axis1, axis2, flattened],
+  'sum': [axis1, axis2, flattened]
+}
