2031795dfb22653e042781bebb0ba817eb8e01ce
xan <47989552+style-x@users.noreply.github.com>
2023-03-26 18:09:01 +0200

    numpy dont work this way

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,5 +1,12 @@
+import numpy as np
 
-{
+def calculate(list):
+
+  axis1 = int(list[3]),int(list[4]),int(list[5])
+  axis2 = int(list[1]),int(list[5]),int(list[7])
+  flattened = 0
+  
+  return {
   'mean': [axis1, axis2, flattened],
   'variance': [axis1, axis2, flattened],
   'standard deviation': [axis1, axis2, flattened],
